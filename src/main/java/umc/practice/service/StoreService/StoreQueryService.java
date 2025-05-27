afeeb0c24a78ef0d8f50fe4ae531a4e82055f03e
yenalee <lyemee@naver.com>
2025-05-27 16:20:22 +0900

    view review list API

--- src/main/java/umc/practice/service/StoreService/StoreQueryService.java
+++ src/main/java/umc/practice/service/StoreService/StoreQueryService.java
@@ -1,5 +1,7 @@
 package umc.practice.service.StoreService;
 
+import org.springframework.data.domain.Page;
+import umc.practice.domain.Review;
 import umc.practice.domain.Store;
 
 import java.util.List;
@@ -9,4 +11,8 @@ public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
     List<Store> findStoresByNameAndScore(String name, Float score);
+
+    // 9th 실습
+    Page<Review> getReviewList(Long StoreId, Integer page);
+
 }
--- src/main/java/umc/practice/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/practice/service/StoreService/StoreQueryServiceImpl.java
@@ -1,9 +1,13 @@
 package umc.practice.service.StoreService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.practice.domain.Review;
 import umc.practice.domain.Store;
+import umc.practice.repository.ReviewRepository;
 import umc.practice.repository.StoreRepository.StoreRepository;
 
 import java.util.List;
@@ -15,6 +19,9 @@ import java.util.Optional;
 public class StoreQueryServiceImpl implements StoreQueryService{
 
     private final StoreRepository storeRepository;
+
+    // 9th 실습..? reviewCommandServiceImpl과 겹침..(워크북과 달라서)
+    private final ReviewRepository reviewRepository;
 
     @Override
     public Optional<Store> findStore(Long id) {
@@ -28,5 +35,13 @@ public class StoreQueryServiceImpl implements StoreQueryService{
         filteredStores.forEach(store -> System.out.println("Store: " + store));
 
         return filteredStores;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+        Store store = storeRepository.findById(StoreId).get();
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return StorePage;
     }
 }
